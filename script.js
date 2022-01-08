bcb0fd6e01ddc3bbfc7d6e33c2894ddd172bdff1
Alexey Shevchenko <90980761+Al3x3ySh3vch3nko@users.noreply.github.com>
2022-01-08 22:09:29 +0300

    +

--- script.js
+++ script.js
@@ -79,26 +79,26 @@ document.addEventListener('keydown', function (event)
 // random color
 ///////////////////////////////////////
 
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min +1) + min)
-const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`
+// const randomInt = (min, max) => Math.floor(Math.random() * (max - min +1) + min)
+// const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`
 
-console.log(randomColor(0, 255))
+// console.log(randomColor(0, 255))
 
-document.querySelector('.nav__link').addEventListener('click', function (event)
-{
-    this.style.backgroundColor = randomColor()
-    event.stopPropagation()
-})
+// document.querySelector('.nav__link').addEventListener('click', function (event)
+// {
+//     this.style.backgroundColor = randomColor()
+//     event.stopPropagation()
+// })
 
-document.querySelector('.nav__links').addEventListener('click', function (event)
-{
-    this.style.backgroundColor = randomColor()
-})
+// document.querySelector('.nav__links').addEventListener('click', function (event)
+// {
+//     this.style.backgroundColor = randomColor()
+// })
 
-document.querySelector('.nav').addEventListener('click', function (event)
-{
-    this.style.backgroundColor = randomColor()
-})
+// document.querySelector('.nav').addEventListener('click', function (event)
+// {
+//     this.style.backgroundColor = randomColor()
+// })
 
 ///////////////////////////////////////
 // page navigation
